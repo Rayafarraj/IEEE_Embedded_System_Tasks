a05a10275c482c6dc6cc532d834222b2b770a3a4
Rayafarraj <149169313+Rayafarraj@users.noreply.github.com>
2026-03-16 21:30:19 +0200

    task2.c

--- task2.c
+++ task2.c
@@ -1,1 +1,18 @@
-task
+#include <stdio.h>
+
+int ToggleBit(int x, int position)
+{
+    int answer;
+    answer = x ^ (1 << position);
+    return answer;
+}
+
+int main()
+{
+    int x = 10;
+    int position = 1; 
+    printf("Original value: %d\n", x);
+    x = ToggleBit(x, position);
+    printf("After toggling bit %d: %d\n", position, x);
+    return 0;
+}
